9477b2095415835afa91b591b0794190bee456a0
Dmitriy <95078422+Amid3222@users.noreply.github.com>
2025-03-13 14:15:36 +0300

    test

--- src/com/walking/lesson1_varaibles/Task1.java
+++ src/com/walking/lesson1_varaibles/Task1.java
@@ -4,7 +4,7 @@ package com.walking.lesson1_varaibles;
  * На месте комментария написать код, который присвоит переменной a значение переменной b и наоборот.
  * Подсказка: можно использовать третью переменную.
  */
-public class Task1 {
+public class  Task1 {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
